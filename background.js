a4beb73c58e91571a4eb247a9074e8922f40fec8
Mike Conley <mike.d.conley@gmail.com>
2023-10-05 10:04:28 -0400

    Switch from @'ing Firefox to #'ing it instead.

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const HEADER = "Not Wordle, just my @firefox tabs:";
+const HEADER = "Not Wordle, just my #firefox tabs:";
 const FOOTER = "https://mzl.la/3sfOF1R";
 const MAX_CHARS = 280; // Twitter default
 
